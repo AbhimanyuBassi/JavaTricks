18d0b3b2ed8d20d1244652b835e02a111b286b28
AbhimanyuBassi <abhimanyu.bassi@gmail.com>
2022-07-26 18:10:25 +0800

    Singleton implementation - call Explanation of stream implementation

--- src/work/maxSalaryEachDept/EmpDeptDriver.java
+++ src/work/maxSalaryEachDept/EmpDeptDriver.java
@@ -20,6 +20,12 @@ public class EmpDeptDriver {
         employeeDetailsList.add(new EmployeeDetails("Creed", "Tech", 15000));
         employeeDetailsList.add(new EmployeeDetails("Tom", "Tech", 15000));
 
+        //This code creates a Stream of all the employees and groups them with their department with the help of Collectors.groupingBy.
+        // For all the values classified to the same key, we need to keep only the employee with the maximum salary,
+        // so we collect them with Collectors.maxBy and the comparator compares the salary with Comparator.comparingInt.
+        // Since maxBy returns an Optional<Employee> (to handle the case where there the list is empty),
+        // we wrap it with a call to Collectors.collectingAndThen with a finisher that just returns the employee:
+        // we know in this case that the optional won't be empty.
 
         Map<String, EmployeeDetails> topEmployees =
                 employeeDetailsList.stream()
---
+++ src/work/singleton/SingletonDriver.java
@@ -0,0 +1,21 @@
+package work.singleton;
+
+public class SingletonDriver {
+    public static void main(String[] args) {
+        Singleton x = Singleton.getInstance();
+
+        Singleton y = Singleton.getInstance();
+
+        // Instantiating Singleton class with variable z
+        Singleton z = Singleton.getInstance();
+
+        // Printing the hash code for above variable as
+        // declared
+        System.out.println("Hashcode of x is "
+                + x.hashCode());
+        System.out.println("Hashcode of y is "
+                + y.hashCode());
+        System.out.println("Hashcode of z is "
+                + z.hashCode());
+    }
+}
