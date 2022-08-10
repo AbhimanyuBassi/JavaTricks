3594a648c59bb582f33e7414cc4b7c3c8a402a07
AbhimanyuBassi <abhimanyu.bassi@gmail.com>
2022-08-10 19:39:46 +0800

    HashMapDemo - initial Checkout

---
+++ src/work/collections/hashmap/HashMapDemo.java
@@ -0,0 +1,10 @@
+package work.collections.hashmap;
+
+public class HashMapDemo {
+
+    //if you want to preserve the order of mapping, then you can consider using LinkedHashMap.
+    // If you wish to keep mappings sorted, then you can use TreeMap, which is a sorted map implementation.
+    // If you need a hash table implementation that is thread-safe and can be used in a concurrent application
+    // without compromising the Scalability, then consider using a ConcurrentHashMap
+
+}
